379b3d0c448016cfed6068126959b7981c2f9cb9
Gleb Gladkiy <gleba@yandex.ru>
2018-07-10 14:27:46 +0300

    feat(server): added /lya

--- server/handlers.js
+++ server/handlers.js
@@ -1,7 +1,11 @@
 const help = require('./help.js');
 const rstv = require('./rstv.js');
 
+const SendResponse = require('./sendResponse');
+
 module.exports = bot => {
+  const sendResponse = SendResponse(bot);
+
   bot.onText(/^\/start$/, msg => {
     if (msg.chat.type === 'private') {
       bot.sendMessage(
@@ -13,6 +17,14 @@ module.exports = bot => {
         `
       );
     }
+  });
+
+  bot.onText(/^\/lya (-?\d+\.?\d*) (-?\d+\.?\d*) ?(\d*)$/, (msg, match) => {
+    sendResponse(
+      msg,
+      `https://yandex.ru/maps/?ll=${match[2]}%2C${match[1]}&z=${match[3] ||
+        '10'}`
+    );
   });
 
   help(bot);
